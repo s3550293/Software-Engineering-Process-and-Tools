f66161253d502cdd92725cb4b933553e8214b85d
rmit-s3550293-joseph-garner <s3550293@student.rmit.edu.au>
2017-03-03 19:12:38 +1100

    Foundation

---
+++ src/Human.java
@@ -0,0 +1,11 @@
+
+public class Human {
+	public Human()
+	{
+		//Code
+	}
+	public void print()
+	{
+		System.out.println("Hello World");
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Main File");
+		Human person = new Human();
+		person.print();
+		
+	}
+}

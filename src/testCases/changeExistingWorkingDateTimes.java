ee192ef23106b01a4a6e6d1f06a252da0618b614
rmit-s3543202-panhaseth-heang <s3543202@student.rmit.edu.au>
2017-03-14 22:27:34 +1100

    Add 'Out of Bound' Checking when assigning working hours to employee

--- src/testCases/changeExistingWorkingDateTimes.java
+++ src/testCases/changeExistingWorkingDateTimes.java
@@ -2,24 +2,43 @@ package testCases;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import java.text.*;
+import java.util.*;
+import org.junit.*;
 
 public class changeExistingWorkingDateTimes {
 
 	//existing id
 	private static int[] employeeid = {1,2,3,4,5};
+	private static String startinghour = "08:00 AM";
+	private static String endinghour = "05:00 PM";
 	
+	 
 	
 	@Before
 	public void setUp() throws Exception {
-		//checkExistingIDTest(1);
+		
+	}
+	
+	public Boolean checkOutofBoundWorkinghours(String start, String end) throws ParseException{
+		
+		SimpleDateFormat formatTime = new SimpleDateFormat ("hh:mm aa");
+		Date startBound = formatTime.parse(startinghour);
+		Date endBound = formatTime.parse(endinghour);
+		Date startTime = formatTime.parse(start);
+		Date endTime = formatTime.parse(end);
+		
+		//check if input time is within regular working hours 8:00 AM - 5:00 PM
+		if((startTime.getTime() - startBound.getTime() >= 0) 
+				&& (endBound.getTime() - endTime.getTime() >= 0)){
+			return true;
+		}
+		return false;
 	}
 	
 	public boolean checkEmployeeIDTest(int userInput) {
+		
+		//check if id matches
 		for (int i : employeeid){
 			if(i == userInput){
 				return true;
@@ -27,7 +46,7 @@ public class changeExistingWorkingDateTimes {
 		}
 		return false;
 	}
-
+	
 	@After
 	public void tearDown() throws Exception {
 	}
@@ -43,14 +62,17 @@ public class changeExistingWorkingDateTimes {
 	}
 	
 	@Test
-	public void changeExistingTime(){
+	public void checkTimeOutofBound() throws ParseException{
+		String start = "09:00 AM";
+		String end = "04:00 PM";
+		assertTrue(checkOutofBoundWorkinghours(start, end));
+	}
+	@Test
+	public void changeExistingTime() throws ParseException{
 		idExistsTest();
-		
+		checkTimeOutofBound();
 		
 	}
 	
 	
-
-	
-
 }

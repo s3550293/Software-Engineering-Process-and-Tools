0cc7daa22104247b65a0a958b845792bc55cdf2d
s3550293 <s3550293@student.rmit.edu.au>
2017-03-18 09:54:50 +1100

    Revert "Revert "Merging Bryans code into the master""

--- src/User.java
+++ src/User.java
@@ -20,4 +20,13 @@ public class User
 	{
 		return _username + " " + _password + " " + _accountType;
 	}
+	public String getUsername() 
+	{
+		
+		return _username;
+	}
+	public String getPassword() 
+	{
+		return _password;
+	}
 }

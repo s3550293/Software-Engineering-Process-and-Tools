93cfdb3237e767ba4f7d0a5e164cb8cf6ce4a013
rmit-s3543202-panhaseth-heang <s3543202@student.rmit.edu.au>
2017-04-04 19:27:58 +1000

    break codes in displaying booking to smaller functions

--- assignment/src/program/Controller.java
+++ assignment/src/program/Controller.java
@@ -364,7 +364,7 @@ public class Controller
 	}*/
 
 	/**
-	 * @author Luke Mason, David Heang
+	 * @author Luke Mason, David(Panhaseth Heang)
 	 * UI for adding employee working times for next month
 	 * @param employeeId
 	 * @return only returns false if user somehow breaks out of infinite loop with out exiting function
@@ -1264,6 +1264,12 @@ public class Controller
 		
 	}
 
+	/**
+	 * @author Bryan Soh, David(Panhaseth Heang)
+	 * 
+	 * @param 
+	 * @return integer(book id)
+	 */
 	public int checkNextBooking_GetBookID()
 	{
 		boolean loopflag = true;
@@ -1286,101 +1292,133 @@ public class Controller
 				nDays[i] = today;
 			}
 			
-			System.out.printf("\n%s", "ID");
-			System.out.printf("%-2s %s", "", "Customer ID");
-			System.out.printf("%-20s %s", "", nDays[0]);
-			System.out.printf("%-3s %s", "", nDays[1]);
-			System.out.printf("%-3s %s", "", nDays[2]);
-			System.out.printf("%-3s %s", "", nDays[3]);
-			System.out.printf("%-3s %s", "", nDays[4]);
-			System.out.printf("%-3s %s", "", nDays[5]);
-			System.out.printf("%-3s %s\n", "", nDays[6]);
-			System.out.print(
-					"-------------------------------------------------------------------------------------------------------------------------------------");
-			for (Booking b : bookList)
-			{
-				System.out.printf("\n%d %-2s %-20s", b.getBookingID(), "", b.getCustomerId());
-				
-				for (int j = 0; j < 7; j++)
-				{
-					String bookedDays=convertDateToString(b.getDate());
-					if (!bookList.isEmpty())
-					{
-						if (nDays[j].equals(bookedDays) && b.getStatus().equals("active"))
-						{
-							System.out.printf("%-8s %-5s", "", "Booked");
-							
-						}
-						else
-						{
-							System.out.printf("%-8s %-5s", "", "-----");
-						}
-					} else
-					{
-						System.out.printf("%-8s %-5s", "", "-----");
-					}
-				}
-			}
+			displayDetailedBooking_Date(bookList, nDays);
 			
 			Booking bookings = new Booking();
 			boolean tryLoop = true;
-			String input;
-			int bookKey = 0;
-			do
-			{
+			do{
+				String input;
+				int bookKey = 0;
 				System.out.println("\nPlease enter booking id to view more or 'quit' to quit");
 				input = sc.nextLine();
-				if (input.equalsIgnoreCase("quit"))
-				{
-					Business bo = new Business();
-					bo.companyMenu();
-				} 
-				else
-				{
-					try
-					{
-						bookKey = Integer.parseInt(input);
-						tryLoop = false;
-					} catch (NullPointerException e)
-					{
-						System.out.println("Invalid Input");
-					}
+				if (input.equalsIgnoreCase("quit")) {
+					return 0;
 				}
-			} while (tryLoop);
-			bookings = connect.getOneBooking(bookKey);
-			System.out.printf("\nBookID: %-15s CusID: %-2d\n", bookings.getBookingID(), bookings.getCustomerId());
-			System.out.printf("\n%-15s %-15s %s\n", "Date", "Start Time", "End Time");
-			System.out.println("----------------------------------------------------");
+				
+				try 
+				{ 
+					  Integer.parseInt(input); 
+				} 
+				catch(NumberFormatException e) 
+				{
+					  	tryLoop=true;
+					  	System.out.println("Invalid Input");
+						break; 
+				}
+				bookKey = Integer.parseInt(input);
+				displayDetailedBooking_StartEndTime(bookKey,bookings, input,bookList,nDays,tryLoop,loopflag);
+						
+				return bookKey;
+			}while(tryLoop);
+		}
+		return 0;
+	}
+	
+	/**
+	 * @author Bryan Soh, David(Panhaseth Heang)
+	 * Display date overview table of booking for 7 days
+	 * @param 
+	 * @return 
+	 */
+	public void displayDetailedBooking_Date(ArrayList<Booking> bookList, String nDays[]){
+		System.out.printf("\n%s", "ID");
+		System.out.printf("%-2s %s", "", "Customer ID");
+		System.out.printf("%-20s %s", "", nDays[0]);
+		System.out.printf("%-3s %s", "", nDays[1]);
+		System.out.printf("%-3s %s", "", nDays[2]);
+		System.out.printf("%-3s %s", "", nDays[3]);
+		System.out.printf("%-3s %s", "", nDays[4]);
+		System.out.printf("%-3s %s", "", nDays[5]);
+		System.out.printf("%-3s %s\n", "", nDays[6]);
+		System.out.print(
+				"----------------------------------------------------------------------------------------------------------------------------------");
+		
+		for (Booking b : bookList)
+		{
+			System.out.printf("\n%d %-2s %-24s", b.getBookingID(), "", b.getCustomerId());
+			
 			for (int j = 0; j < 7; j++)
 			{
-				System.out.printf("%s", nDays[j]);
-				if (bookings!=null)
+				String bookedDays=convertDateToString(b.getDate());
+				if (!bookList.isEmpty())
 				{
-					if (nDays[j].equals(convertDateToString(bookings.getDate())))
+					if (nDays[j].equals(bookedDays) && b.getStatus().equals("active"))
 					{
-						if(bookings.getStatus().equals("active")){
-							String startTime=convertTimeToString(bookings.getStartTime());
-							String endTime=convertTimeToString(bookings.getEndTime());
-							System.out.printf("%6s %-15s %s\n", "",startTime,
-									endTime);
-						}else
-						{
-							System.out.printf("%6s %-15s %s\n", "", "-----", "-----");
-						}
-					} else
+						System.out.printf("%-8s %-5s", "", "Booked");
+						
+					}
+					else
 					{
-						System.out.printf("%6s %-15s %s\n", "", "-----", "-----");
+						System.out.printf("%-8s %-5s", "", "-----");
 					}
 				} else
 				{
-					System.out.printf("%6s %-15s %s\n", "", "-----", "-----");
+					System.out.printf("%-8s %-5s", "", "-----");
 				}
 			}
-			return bookKey;
 		}
-		return 0;	
 	}
 	
+	/**
+	 * @author Bryan Soh, David(Panhaseth Heang)
+	 * A detailed display after user select book id, shows start time and end time of booking
+	 * @param 
+	 * @return 
+	 */
+	public void displayDetailedBooking_StartEndTime(int bookKey, Booking bookings, String input, ArrayList<Booking> bookList, String nDays[], Boolean tryLoop, Boolean loopflag)
+	{
+		
+		DatabaseConnection connect = new DatabaseConnection();
+		
+		for (int b = 0; b < bookList.size(); b++) {
+			if (bookList.get(b).getBookingID() == bookKey) {
+				bookings = connect.getOneBooking(bookKey);
+				System.out.printf("\nBookID: %-15s CusID: %-2s\n", bookings.getBookingID(),
+						bookings.getCustomerId());
+				System.out.printf("\n%-15s %-15s %s\n", "Date", "Start Time", "End Time");
+				System.out.println("----------------------------------------------------");
+				for (int j = 0; j < 7; j++) {
+					System.out.printf("%s", nDays[j]);
+					if (bookings != null) {
+						if (nDays[j].equals(convertDateToString(bookings.getDate()))) {
+							String startTime = convertTimeToString(bookings.getStartTime());
+							String endTime = convertTimeToString(bookings.getEndTime());
+							System.out.printf("%6s %-15s %s\n", "", startTime, endTime);
+						} else {
+							System.out.printf("%6s %-15s %s\n", "", "-----", "-----");
+						}
+					} else {
+						System.out.printf("%6s %-15s %s\n", "", "-----", "-----");
+					}
+				}
+				tryLoop = false;
+				b = bookList.size();
+			}
+		}
+		if(tryLoop)
+		{
+			System.out.println("Invalid Input");
+			loopflag=true;
+			tryLoop = false;
+		}	
+	}
+	
+	/**
+	 * @author David(Panhaseth Heang)
+	 * get booking id and set its status to 'cancel'
+	 * @param 
+	 * @return
+	 */
 	public void cancelBooking(){
 		DatabaseConnection conn = new DatabaseConnection();
 		Scanner sc = new Scanner(System.in);
@@ -1388,6 +1426,9 @@ public class Controller
 		
 		do{
 			int bookID = checkNextBooking_GetBookID();
+			if(bookID == 0){
+				return;
+			}
 			System.out.println("\nPlease enter 'cancel' to cancel book id " + bookID + " or 'return'");
 			String input = sc.nextLine();
 			if(input.equalsIgnoreCase("cancel")){

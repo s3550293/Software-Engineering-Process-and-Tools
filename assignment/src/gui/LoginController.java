f647d3bbc1d0e491dd17c1248a30fc1c5711b82c
rmit-s3550293-joseph-garner <s3550293@student.rmit.edu.au>
2017-04-21 10:02:52 +1000

    Added error lable to login window
    set the lable visibility to false

--- assignment/src/gui/LoginController.java
+++ assignment/src/gui/LoginController.java
@@ -1,21 +1,25 @@
 package gui;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-public class LoginController {
+public class LoginController implements Initializable {
 	public static Logger log = Logger.getLogger(LoginController.class);
 	
 	@FXML
@@ -24,6 +28,13 @@ public class LoginController {
 	PasswordField txtPassLogin;
 	@FXML
 	Button btnLogin, btnRegister;
+	@FXML
+	Label lblError;
+	
+	public void initialize(URL url, ResourceBundle rb)
+	{
+		lblError.setVisible(false);
+	}
 	
 	/**
 	 * Logs User in

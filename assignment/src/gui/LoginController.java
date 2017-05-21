4c1c8fb70b2c9170a37aee3fc4555c3e70146898
Luke Mason <s3630120@student.rmit.edu.au>
2017-05-21 12:56:18 +1000

    Added test data for logging into wrong business

--- assignment/src/gui/LoginController.java
+++ assignment/src/gui/LoginController.java
@@ -219,8 +219,9 @@ public class LoginController implements Initializable {
 		connect.addUser("Hannah", "Apples22", 0,2); // customer attached to Business 2
 		connect.addUserDetails(3, "William", "Porter", "will@mail.com", "0452368593", "01/01/2002", "Male");
 		connect.addUserDetails(4, "Hannah", "Hawlking", "hannah@mail.com", "0452136859", "20/04/1995", "Famale");
-		connect.addEmployee("Luke Charles",25,1);
-		connect.addEmployee("David Smith",26.6,1);
+		connect.addUser("Test1234", "Apples22", 0,6);
+		connect.addEmployee("Luke Charles",25,2);
+		connect.addEmployee("David Smith",26.6,2);
 		connect.addEmployee("Will Turner",15,2);
 		connect.addEmployee("Rob Pointer",14,2);
 		connect.addEmployee("Adam Mason",12,3);
